525a2d43eb05b87347f994ddb7f50fa5c360dfc1
redhaputra <putra.redha@gmail.com>
2017-11-30 13:21:57 +0700

    add ocr post

--- orcestrator/app.js
+++ orcestrator/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 // const dotenv = require('dotenv').config()
 const cors = require('cors')
 const users = require('./routes/users')
+const ocr = require('./routes/ocr')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
 
 app.use('/users', users)
+app.use('/ocr', ocr)
 
 app.listen(3000, (err) => {
   if(err) {
