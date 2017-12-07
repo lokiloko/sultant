7cb3e33401c86116063c50b040239e134299673c
Ian Winanto <ianw.tkj2@gmail.com>
2017-12-07 13:52:18 +0700

    Delete ocr.js

--- orcestrator/routes/ocr.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-
-const { multer, uploadFile } = require('../helpers/images')
-const controller = require('../controllers/ocrController')
-
-router.post('/', multer.single('image'), uploadFile, controller.postOcr)
-
-module.exports = router
